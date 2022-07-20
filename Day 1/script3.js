7c11dbe942b5d230fc4cf1cf0763dbfc6de19e74
Biswajit Basak <juelbasak@gmail.com>
2022-07-20 17:34:09 +0530

    Learning JS operators

---
+++ Day 1/script3.js
@@ -0,0 +1,22 @@
+const uid = "abc123";
+
+// uid = "abcd234";
+
+var full_name = "Biswajit Basak";
+var email = "email@email.com";
+var password = "123456";
+var confirm_password = "123456";
+var course_count = 0;
+
+var is_logged_from_google = false;
+var is_logged_from_facebook = false;
+var is_logged_from_github = false;
+
+// full_name = prompt("Enter your name")
+
+// Interpolation
+console.log(full_name);
+console.log(`
+    Name : ${full_name}
+    Email : ${email}`)
+

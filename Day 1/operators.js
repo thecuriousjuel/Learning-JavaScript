e85685f9f9a961408b379eb3721d76ab1486eb6a
Biswajit Basak <juelbasak@gmail.com>
2022-07-20 19:22:49 +0530

    Operators got deleted and re-written again

---
+++ Day 1/operators.js
@@ -0,0 +1,12 @@
+// + - * % **
+let actual_price = 799
+let current_price = 199
+
+let total_price = ((actual_price - current_price) / actual_price) * 100
+
+console.log(total_price)
+console.log(Math.round(total_price))
+
+console.log(total_price.toFixed(2))
+
+console.log(typeof 1)

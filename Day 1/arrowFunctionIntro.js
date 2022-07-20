e9559c80906cd8cebee667d601e6714c8568857c
Biswajit Basak <juelbasak@gmail.com>
2022-07-20 22:25:32 +0530

    Working with functions

---
+++ Day 1/arrowFunctionIntro.js
@@ -0,0 +1,24 @@
+let getUserRole = (name, role) =>
+{
+    switch (role)
+    {
+        case "admin":
+            return `${name} is admin with all access.`
+
+        case "subadmin":
+            return `${name} is subadmin with access to add or delete courses.`
+
+        case "testprep":
+            return `${name} is tester with access test courses.`
+
+        case "user":
+            return `${name} is user with access to watch courses.`
+
+        default:
+            return `${name} is a trial user.`
+    }
+}
+
+out = getUserRole('Biswajit', 'user')
+console.log(out)
+
---
+++ Day 1/methods.js
@@ -0,0 +1,25 @@
+function sayHello(name)
+{
+    console.log(`Hello! ${name}`)
+}
+
+d = sayHello
+
+d('Biswajit')
+
+function namastey()
+{
+    return "Hello! in India"
+}
+
+let out = namastey()
+console.log(out)
+
+
+// out = function namastey() => (
+//     "Hello! in India"
+// )
+
+
+
+
